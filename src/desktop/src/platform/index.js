01f976d835eaf7d71edd50eca83a2d177190b08e
syhx <u202114724@hust.edu.cn>
2024-07-15 17:19:38 +0800

    remove build files

--- src/desktop/src/platform/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logPlatform = logPlatform;
-function logPlatform() {
-    return "Hi,I'm here";
-}
-//# sourceMappingURL=index.js.map
