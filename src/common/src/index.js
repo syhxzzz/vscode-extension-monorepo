916671661db489f633044b8873905329102ed468
syhx <u202114724@hust.edu.cn>
2024-07-15 15:14:12 +0800

    Completing tsconfig on monorepo's path configuration

---
+++ src/common/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.log114514 = log114514;
+function log114514() {
+    console.log(114514);
+    return "114514";
+}
+//# sourceMappingURL=index.js.map
--- src/common/src/index.ts
+++ src/common/src/index.ts
@@ -1,3 +1,4 @@
 export function log114514() {
   console.log(114514);
+  return "114514";
 }
